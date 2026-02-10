8f767eff215be6fcbdee4ed2862d084b9ff4a60c
cmcrespo06 <cmariacrespo00@gmail.com>
2026-02-11 00:17:37 +0100

    Edit nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      title: 'Personajes Rick & Morty',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Dashboard de personajes de Rick and Morty' }
+      ]
+    }
+  },
+
+  typescript: {
+    strict: true,
+    typeCheck: true
+  }
 })
